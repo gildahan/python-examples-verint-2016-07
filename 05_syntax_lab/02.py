e35f18b0584f09f710702bef4c5cd167fb058305
gildahan <gil.dahan@verint.com>
2016-07-12 18:03:56 +0300

    Solution for: sol-python-verint.syntax-lab/02.py

--- 05_syntax_lab/02.py
+++ 05_syntax_lab/02.py
@@ -1,6 +1,17 @@
+from random import randint
 """
-Write a Python program that randomizes 7 numbers
-and prints their sum total.
-If the sum is divisable by 7, also print the word "Boom"
+Print the sum of a 7 random number between 1-100
 """
+newX=0
+i=0
+while  i < 7:
+     x=randint(1,100)
+     print i+1 ,"- select random number "  , x
+     
+     newX+=x
+     i+=1
+    
+print "The sum is: ", newX
 
+if newX % 7 == 0 :
+    print "Deviding By 7 So ....Boom " 
