14e3a07e187201a43a8bca370b3c31cca1e0f0a3
gildahan <gil.dahan@verint.com>
2016-07-12 18:03:52 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,14 @@
+from random import randint
+from array import array
+
 """
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
+Finding a number deviding 15 , 13 ,7
 """
+
+num = randint(1,1000000)
+while (num % 7 != 0) or (num % 13 != 0) or (num % 15 != 0): 
+    print "The current selected numer is : " , num
+    num = randint(1,1000000)
+    
+print "The winner num is :", num
 
