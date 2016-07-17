14734cbc427cff69ce91dd0cd035becdc913c2c9
gildahan <gil.dahan@verint.com>
2016-07-17 17:23:33 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,7 +1,23 @@
-"""
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name
-Program should write the lines from the first two 
-files interwinded into the output file
-"""
+import sys 
 
+(_,fin1,fin2 , fout) = sys.argv
+array1 = []
+array2 = []
+with open(fin1,"r") as f1:
+    for line in f1:
+        array1.append(line)
+with open(fin2,"r") as f2:
+    for line in f2:
+        array2.append(line)
+with open(fout,"a") as f3:
+    if len(array1)>len(array2):
+        for count in range(len(array1)):
+            f3.write(array1[count])
+            if len(array2)>count:
+                f3.write(array2[count])
+    else:
+        for count in range(len(array2)):
+            if len(array1)>count:
+                f3.write(array1[count])
+            f3.write(array2[count])
+            
